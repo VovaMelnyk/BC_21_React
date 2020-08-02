784d572da4e15707cc1913a2ad9b321f83639c55
Volodymyr Melnyk <volodymyr.melnyk@firstbridge.io>
2020-08-03 00:23:17 +0300

    fix counter

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from "react";
 import TodoList from "./Components/TodoList/TodoList";
-import Counter from "./Components/Counter/Counter";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      {/* <TodoList /> */}
-      <Counter title="Counter" />
+      <TodoList />
     </div>
   );
 }
